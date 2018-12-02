94bce3736a907c29e104cd63a200c96148cae61f
jlarson7 <joshua-j-larson@uiowa.edu>
2018-12-01 20:29:05 -0600

    Update mqttEx.js

--- mqttEx.js
+++ mqttEx.js
@@ -1,16 +1,22 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
- 
-client.on('connect', function () {
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }
-  })
-})
- 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+var Cayenne = require('cayennejs');
+
+// Initiate MQTT API
+const cayenneClient = new Cayenne.MQTT({
+  username: "25e51420-f5d1-11e8-a254-e163efaadfe8",
+  password: "a18088c00f74a59d3671ee73e4b8b1cb6f0f2ee4",
+  clientId: "a093f6c0-f5d9-11e8-b82d-f12a91579eed"
+});
+
+cayenneClient.connect((err, mqttClient) => {
+  // dashboard widget automatically detects datatype & unit
+  cayenneClient.kelvinWrite(3, 65);
+
+  // sending raw values without datatypes
+  cayenneClient.rawWrite(4, 123);
+
+  // subscribe to data channel for actions (actuators)
+  cayenneClient.on("cmd9", function(data) {
+    console.log(data);
+  });
+
+});
